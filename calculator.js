3ee20d46f31e0f7c9fe0aa676395742ee28e66e4
Gavin Blair <gavinblair@users.noreply.github.com>
2020-03-28 00:53:49 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -9,109 +9,114 @@ var calculation = {
     },
     'low_multiplier': 0.025,//32.7, // Roughly 2.5% of their operating budget if multiplied by their monthly
     'high_multiplier': 0.03,//39.3, // Roughly 3% of their operating budget if multiplied by their monthly
-    'multiplier': 0.0275 // Companies spend an average of 2.75% (2.5-3%) of their unfunded budget on their website
-};
+    'multiplier': 0.0275, // Companies spend an average of 2.75% (2.5-3%) of their unfunded budget on their website
+    'parseInt': function(value) {
+        return parseInt(value, 10) || 0;
+    },
+    'roundNext500': function(number) {
+        return Math.ceil((this.parseInt(number) + 1) / 500) * 500;
+    },
+    'formatNumber': function(n) {
+        // format number 1234567 to 1,234,567
+        return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");//.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    },
+    'operatingBudget': function() {
+        var operating_budget = this.parseInt(document.getElementsByName(this.budget_field_name)[0].value);
+        // console.log('operating_budget:' + operating_budget);
+        return operating_budget;
+    },
+    'funding': function() {
+        var funded_budget = this.parseInt(document.getElementsByName(this.funding_field_name)[0].value) / 100 * this.operatingBudget();
+        // console.log('funded: ' + funded_budget);
+        return funded_budget;
+    },
+    'unfunded': function() {
+        var unfunded = this.operatingBudget() - this.funding();
+        // console.log('unfunded: ' + unfunded);
+        return unfunded;
+    },
+    'lowRange': function() {
+        var low_range = this.parseInt(this.roundNext500(this.low_multiplier * this.unfunded()));
+        // console.log('low_range: ' + low_range);
+        return low_range
+    },
+    'highRange': function() {
+        var high_range = this.parseInt(this.roundNext500(this.high_multiplier * this.unfunded()));
+        // console.log('high_range: ' + high_range);
+        return high_range;
+    },
+    'monthly': function() {
+        var monthly = this.parseInt(this.roundNext500(this.multiplier * this.unfunded())) / 36; //36: THE MAGIC PACKAGE NUMBER (36 months)
 
+        for (var i in this.packages) {
+            if (monthly <= i) {
+                break;
+            }
+        }
 
-calculation.parseInt = function(value) {
-    return parseInt(value, 10) || 0;
-};
+        return i;
+    },
+    'readableResults': function() {
 
-calculation.roundNext500 = function (number) {
-    return Math.ceil((calculation.parseInt(number) + 1) / 500) * 500;
-};
+        var low = this.lowRange();
+        var high = this.highRange();
 
-calculation.formatNumber = function (n) {
-    // format number 1234567 to 1,234,567
-    return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");//.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-};
+        var range = "approximately <strong>$" + this.formatNumber(low) + " - $" + this.formatNumber(high) + " every 3-5 years</strong>";
 
-calculation.operatingBudget = function() {
-    var operating_budget = calculation.parseInt(document.getElementsByName(calculation.budget_field_name)[0].value);
-    // console.log('operating_budget:' + operating_budget);
-    return operating_budget;
-};
+        if(high <= 10000) {
+            range = "<strong>less than $10,000 every 3-5 years</strong>";
+        } else if (high >= 125000) {
+            range = "<strong>more than $125,000 every 3-5 years</strong>";
+        }
 
-calculation.funding = function() {
-    var funded_budget = calculation.parseInt(document.getElementsByName(calculation.funding_field_name)[0].value) / 100 * calculation.operatingBudget();
-    // console.log('funded: ' + funded_budget);
-    return funded_budget;
-};
+        var plan = "<strong>" + this.packages[this.monthly()] + "</strong>";
 
-calculation.unfunded = function () {
-    var unfunded = calculation.operatingBudget() - calculation.funding();
-    // console.log('unfunded: ' + unfunded);
-    return unfunded;
-};
+        //calculator results
+        return "Results:<br />&nbsp;<br />Based on our market research and data, your website budget would be "
+            + range + " based on a traditional billing structure.<br />&nbsp;<br />Using our equalized package pricing,"
+            + " we would recommend: <br />" + plan;//\n\nSupport: up to $170/month";
+    },
+    'showResults': function() {
 
-calculation.lowRange = function() {
-    var low_range = calculation.parseInt(calculation.roundNext500(calculation.low_multiplier * calculation.unfunded()));
-    // console.log('low_range: ' + low_range);
-    return low_range
-};
+        var results = this.readableResults();
 
-calculation.highRange = function() {
-    var high_range = calculation.parseInt(calculation.roundNext500(calculation.high_multiplier * calculation.unfunded()));
-    // console.log('high_range: ' + high_range);
-    return high_range;
-};
+        //human readable results
+        document.getElementById("calculation_results").innerHTML = results;
+        document.getElementsByName("TICKET.content")[0].value = results.replace("<strong>", "").replace("</strong>", "").replace("<br />", "\n");
 
-calculation.monthly = function() {
-    var monthly = calculation.parseInt(calculation.roundNext500(calculation.multiplier * calculation.unfunded())) / 36; //36: THE MAGIC PACKAGE NUMBER (36 months)
+        setTimeout(function() {
+            if (document.getElementsByClassName('submitted-message').length) {
+                gtag('event', 'calculate', {'event_category': 'calculator'});
+                gtag_report_conversion();
+                gtag('event', 'proposal-request', {'event_category': 'calculator'});
 
-    for (var i in calculation.packages) {
-        if (monthly <= i) {
-            break;
+                //show the results
+                document.getElementsByTagName("BODY")[0].classList.add("show-results");
+            }
+        }, 500);
+
+    },
+    'init': function(){
+        setTimeout(function() {
+            console.log(this.budget_field_name);/**/
+            document.getElementsByName(this.budget_field_name)[0].style.width = "200px";
+            document.getElementsByName(this.funding_field_name)[0].style.width = "200px";
+
+            var theform = document.getElementsByTagName('form')[0];
+            theform.addEventListener("submit", this.submit);
+        }.bind(this), 2000); //timeout
+    },
+    'submit': function(){
+        if (document.getElementsByName(this.budget_field_name)[0]) {
+            this.showResults();
         }
     }
-
-    return i;
 };
+calculation.init();
 
-calculation.readableResults = function() {
-
-    var low = calculation.lowRange();
-    var high = calculation.highRange();
-
-    var range = "approximately <strong>$" + calculation.formatNumber(low) + " - $" + calculation.formatNumber(high) + " every 3-5 years</strong>";
-
-    if(high <= 10000) {
-        range = "<strong>less than $10,000 every 3-5 years</strong>";
-    } else if (high >= 125000) {
-        range = "<strong>more than $125,000 every 3-5 years</strong>";
-    }
-
-    var plan = "<strong>" + calculation.packages[calculation.monthly()] + "</strong>";
-
-    //calculator results
-    return "Results:<br />&nbsp;<br />Based on our market research and data, your website budget would be "
-        + range + " based on a traditional billing structure.<br />&nbsp;<br />Using our equalized package pricing,"
-        + " we would recommend: <br />" + plan;//\n\nSupport: up to $170/month";
-};
-
-calculation.showResults = function() {
-
-    var results = calculation.readableResults();
-
-    //human readable results
-    document.getElementById("calculation_results").innerHTML = results;
-    document.getElementsByName("TICKET.content")[0].value = results.replace("<strong>", "").replace("</strong>", "").replace("<br />", "\n");
-
-    setTimeout(function() {
-        if (document.getElementsByClassName('submitted-message').length) {
-            gtag('event', 'calculate', {'event_category': 'calculator'});
-            gtag_report_conversion();
-            gtag('event', 'proposal-request', {'event_category': 'calculator'});
-
-            //show the results
-            document.getElementsByTagName("BODY")[0].classList.add("show-results");
-        }
-    }, 500);
-
-};
 
 function gtag_report_conversion(url) {
-    var callback = function () {
+    var callback = function() {
         if (typeof(url) !== 'undefined') {
             window.location = url;
         }
@@ -122,20 +127,3 @@ function gtag_report_conversion(url) {
     });
     return false;
 }
-
-
-
-setTimeout(function () {
-
-    document.getElementsByName(calculation.budget_field_name)[0].style.width = "200px";
-    document.getElementsByName(calculation.funding_field_name)[0].style.width = "200px";
-
-    var theform = document.getElementsByTagName('form')[0];
-    theform.addEventListener("submit", function () {
-        if (document.getElementsByName(calculation.budget_field_name)[0]) {
-            calculation.showResults();
-        }
-    });
-}, 1000); //timeout
-
-
